bf0cf51b983642a79456d2308124829749344f64
Brian Manning <brian@xaoc.org>
2014-12-01 14:40:07 -0800

    helloworld_gtk3.pl: revert static method calls that use arrow '->'

--- helloworld_gtk3.pl
+++ helloworld_gtk3.pl
@@ -9,10 +9,12 @@ my $window = Gtk3::Window->new (q(toplevel));
 # create the button
 my $button = Gtk3::Button->new (q(Quit));
 # connect the button's 'click' signal to an action
-$button->signal_connect (clicked => sub { Gtk3->main_quit });
+# Static method calls always use double colons, not the '->' arrow
+$button->signal_connect (clicked => sub { Gtk3::main_quit });
 # add the button to the window
 $window->add ($button);
 # show the window
 $window->show_all;
 # yield to Gtk3 and wait for user input
-Gtk3->main;
+# Static method calls always use double colons, not the '->' arrow
+Gtk3::main;
