03fa64850889fd979cf5af826d083070b7b9f0c5
Brian Manning <brian@xaoc.org>
2014-12-01 06:33:21 -0800

    helloworld_gtk3.pl: fix incorrect calls to Gtk3
    - Call Gtk3 with either 'use Gtk3 q(-init);' or 'use Gtk3;
      Gtk3->init;', or bad things will happen

--- helloworld_gtk3.pl
+++ helloworld_gtk3.pl
@@ -2,17 +2,17 @@
 
 use strict;
 # Gtk3->init; works if you don't use -init on use
-use Gtk3;
+use Gtk3 q(-init);
 
 # create the window
 my $window = Gtk3::Window->new (q(toplevel));
 # create the button
 my $button = Gtk3::Button->new (q(Quit));
 # connect the button's 'click' signal to an action
-$button->signal_connect (clicked => sub { Gtk3::main_quit });
+$button->signal_connect (clicked => sub { Gtk3->main_quit });
 # add the button to the window
 $window->add ($button);
 # show the window
 $window->show_all;
 # yield to Gtk3 and wait for user input
-Gtk3::main;
+Gtk3->main;
